3a68a09b721da3b3bbfd9bfdf17d8f7e152fe8fa
Kevin <arthurkevin1260@gmail.com>
2023-05-16 11:25:49 +0100

    Deploying

---
+++ src/Components/Navbar.js
@@ -0,0 +1,17 @@
+const Navbar = () => {
+    return (  
+        <div>
+<nav>
+    <ul className="navItems">
+        <li>Home</li>
+        <li>Sports</li>
+        <li>Business</li>
+        <li>Entertainment</li>
+        <li>Others</li>
+    </ul>
+</nav>
+        </div>
+    );
+}
+ 
+export default Navbar;
